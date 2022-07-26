4ebbb35f6ecf0e31436fe9a7f48d2cd64faec6a7
bl1ndy <ayoblindstone@gmail.com>
2022-07-26 20:42:04 +0300

    feat: add new/edit actions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,23 @@
 # frozen_string_literal: true
 
 class QuestionsController < ApplicationController
+  before_action :set_question, only: %i[show edit]
+
   def index
     @questions = Question.all
   end
 
-  def show
+  def show; end
+
+  def new
+    @question = Question.new
+  end
+
+  def edit; end
+
+  private
+
+  def set_question
     @question = Question.find(params[:id])
   end
 end
