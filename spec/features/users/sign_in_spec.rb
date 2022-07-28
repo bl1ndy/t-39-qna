aaa49f929d2173b7ab465b8f8d552865797c6b96
bl1ndy <ayoblindstone@gmail.com>
2022-07-28 21:34:44 +0300

    feat: add navigation menu
    Add user sign out spec.
    Add sign_in/sign_out links.

--- spec/features/users/sign_in_spec.rb
+++ spec/features/users/sign_in_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 feature 'User can sign in.', %(
   In order to ask questions
   As an authenticated user
-  I'd like to ba able to sign in
+  I'd like to be able to sign in
 ) do
   given(:user) { create(:user) }
 
@@ -14,7 +14,7 @@ feature 'User can sign in.', %(
   scenario 'Registered user tries to sign in' do
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
-    click_on 'Log in'
+    click_button 'Log in'
 
     expect(page).to have_content('Signed in successfully.')
   end
@@ -22,7 +22,7 @@ feature 'User can sign in.', %(
   scenario 'Unregistered user tries to sign in' do
     fill_in 'Email', with: 'wrong@test.com'
     fill_in 'Password', with: '12345678'
-    click_on 'Log in'
+    click_button 'Log in'
 
     expect(page).to have_content('Invalid Email or password.')
   end
