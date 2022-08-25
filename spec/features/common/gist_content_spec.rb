eb1e6bcbc7fd7fcdc3b10b52fa646c913b6b6491
bl1ndy <ayoblindstone@gmail.com>
2022-08-25 23:13:18 +0300

    test: add Interceptor to mock third-party requests

--- spec/features/common/gist_content_spec.rb
+++ spec/features/common/gist_content_spec.rb
@@ -11,38 +11,70 @@ feature 'User can see gist content', %(
   given(:question) { create(:question, user:) }
   given(:gist_url) { 'https://gist.github.com/bl1ndy/b5d79c9d5e7a312edd5fb27c67f69b68' }
 
-  background do
-    sign_in(user)
-    visit question_path(question)
+  describe 'After Question page load' do
+    background do
+      question.links.create(title: 'My gist', url: gist_url)
+
+      visit question_path(question)
+    end
+
+    scenario 'User sees gist content on question link' do
+      within '.question' do
+        expect(page).to have_content('mocked test gist')
+      end
+    end
   end
 
-  scenario 'User sees gist content on question link' do
-    within '.question' do
-      click_link('Edit')
-      click_link('Add link')
+  describe 'On Question page' do
+    background do
+      sign_in(user)
+      visit question_path(question)
+    end
 
-      within '#question-links' do
+    scenario 'User sees gist content on question link after update question' do
+      within '.question' do
+        click_link('Edit')
+        click_link('Add link')
+
+        within '#question-links' do
+          fill_in(placeholder: 'title', with: 'My gist')
+          fill_in(placeholder: 'url', with: gist_url)
+        end
+
+        click_button('Save')
+
+        expect(page).to have_content('mocked test gist')
+      end
+    end
+
+    scenario 'User sees gist content on answer link after create answer' do
+      within '.new-answer' do
+        fill_in('Text', with: 'My Answer')
         fill_in(placeholder: 'title', with: 'My gist')
         fill_in(placeholder: 'url', with: gist_url)
+
+        click_button('Post Your Answer')
       end
 
-      click_button('Save')
-
-      expect(page).to have_content('Hello, World!')
-    end
-  end
-
-  scenario 'User sees gist content on answer link' do
-    within '.new-answer' do
-      fill_in('Text', with: 'My Answer')
-      fill_in(placeholder: 'title', with: 'My gist')
-      fill_in(placeholder: 'url', with: gist_url)
-
-      click_button('Post Your Answer')
+      within '.answers' do
+        expect(page).to have_content('mocked test gist')
+      end
     end
 
-    within '.answers' do
-      expect(page).to have_content('Hello, World!')
+    scenario 'User sees info message if gist not found' do
+      within '.question' do
+        click_link('Edit')
+        click_link('Add link')
+
+        within '#question-links' do
+          fill_in(placeholder: 'title', with: 'My gist')
+          fill_in(placeholder: 'url', with: 'https://gist.github.com/bl1ndy/b5d79c9d5e7a')
+        end
+
+        click_button('Save')
+
+        expect(page).to have_content('Gist not found')
+      end
     end
   end
 end
