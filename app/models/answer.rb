55e30445ff2c3974334cf9fa921c21ec724e98a8
bl1ndy <ayoblindstone@gmail.com>
2022-08-29 11:39:03 +0300

    refactor: move links association to Linkable concern

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 class Answer < ApplicationRecord
+  include Linkable
+
   belongs_to :user
   belongs_to :question
 
-  has_many :links, as: :linkable, dependent: :destroy
   has_many_attached :files
-
-  accepts_nested_attributes_for :links, reject_if: :all_blank
 
   validates :body, presence: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 class Question < ApplicationRecord
+  include Linkable
+
   belongs_to :user
   belongs_to :best_answer, class_name: 'Answer', optional: true
 
   has_one :reward, dependent: :destroy
 
   has_many :answers, dependent: :destroy
-  has_many :links, as: :linkable, dependent: :destroy
   has_many_attached :files
 
-  accepts_nested_attributes_for :links, reject_if: :all_blank
   accepts_nested_attributes_for :reward, reject_if: :all_blank
 
   validates :title, :body, presence: true
