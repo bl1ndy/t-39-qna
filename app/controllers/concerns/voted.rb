f0d9bb3436cc7231b4747f10e4c9e51505c5e44b
bl1ndy <ayoblindstone@gmail.com>
2022-09-05 10:37:35 +0300

    feat: add check for author's vote

--- app/controllers/concerns/voted.rb
+++ app/controllers/concerns/voted.rb
@@ -5,6 +5,7 @@ module Voted
 
   included do
     before_action :set_votable, only: %i[vote_up vote_down]
+    before_action :check_author_vote, only: %i[vote_up vote_down]
   end
 
   def vote_up
@@ -17,10 +18,6 @@ module Voted
 
   private
 
-  def set_votable
-    @votable = model_klass.find(params[:id])
-  end
-
   def vote(rate)
     vote = @votable.votes.build(rate:, user: current_user)
 
@@ -30,6 +27,24 @@ module Voted
       else
         format.json { render json: vote.errors }
       end
+    end
+  end
+
+  def set_votable
+    @votable = model_klass.find(params[:id])
+  end
+
+  def check_author_vote
+    return unless current_user == @votable.user
+
+    json = {
+      errors: {
+        access: "You can't vote for your own post"
+      }
+    }
+
+    respond_to do |format|
+      format.json { render json: }
     end
   end
 
