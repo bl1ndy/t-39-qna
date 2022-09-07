e20af57698e5b8b4eb1a52572c0a0e3defb8e8c2
bl1ndy <ayoblindstone@gmail.com>
2022-09-07 16:10:34 +0300

    refactor: move vote author validation to model level

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -9,11 +9,16 @@ class Vote < ApplicationRecord
     scope: %i[votable_type user_id],
     message: 'You have already voted for this post'
   }
-  validate :validate_rate_value, on: :create
+  validate :validate_rate_value
+  validate :validate_vote_author
 
   AVAILABLE_RATE_VALUES = [-1, 1].freeze
 
   def validate_rate_value
     errors.add(:rate, 'must be 1 or -1') unless AVAILABLE_RATE_VALUES.include?(rate)
+  end
+
+  def validate_vote_author
+    errors.add(:base, "You can't vote for your own post") if votable&.user == user
   end
 end
