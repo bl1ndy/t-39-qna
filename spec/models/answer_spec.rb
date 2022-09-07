83a2d63bdf56ba495227f90de584957b1c7cdbe7
bl1ndy <ayoblindstone@gmail.com>
2022-09-07 11:58:08 +0300

    test: add Votable shared examples

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require_relative 'concerns/votable'
 
 RSpec.describe Answer, type: :model do
   describe 'associations' do
     it { should belong_to(:question) }
     it { should have_many(:links).dependent(:destroy) }
-    it { should have_many(:votes).dependent(:destroy) }
     it { should have_many_attached(:files) }
 
     it { should accept_nested_attributes_for(:links) }
+
+    it_behaves_like 'votable'
   end
 
   describe 'validations' do
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require_relative 'concerns/votable'
 
 RSpec.describe Question, type: :model do
   describe 'associations' do
@@ -8,13 +9,14 @@ RSpec.describe Question, type: :model do
 
     it { should have_many(:answers).dependent(:destroy) }
     it { should have_many(:links).dependent(:destroy) }
-    it { should have_many(:votes).dependent(:destroy) }
     it { should have_many_attached(:files) }
 
     it { should belong_to(:best_answer).class_name('Answer').optional }
 
     it { should accept_nested_attributes_for(:links) }
     it { should accept_nested_attributes_for(:reward) }
+
+    it_behaves_like 'votable'
   end
 
   describe 'validations' do
