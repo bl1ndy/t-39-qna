368641c5f4793d31850fa249180b17ec8ccfd820
bl1ndy <ayoblindstone@gmail.com>
2022-09-07 16:07:04 +0300

    refactor: include Voted concern into AnswersController
    Add votable concern to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,27 @@
 Rails.application.routes.draw do
+  concern :votable do
+    member do
+      post :vote_up
+      post :vote_down
+      delete :cancel_vote
+    end
+  end
+
   root to: 'questions#index'
 
   devise_for :users
 
   resources :questions do
+    concerns :votable
+
     member do
-      post :vote_up
-      post :vote_down
       delete :destroy_file
       delete :destroy_link
     end
 
     resources :answers, shallow: true, only: %i[create update destroy] do
+      concerns :votable
+
       member do
         post :best
         delete :destroy_file
