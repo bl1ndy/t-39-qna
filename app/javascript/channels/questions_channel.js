ace0d982ba3ced4fea140ef8247551da57b7e07d
bl1ndy <ayoblindstone@gmail.com>
2022-10-03 15:01:26 +0300

    fix: fix multiple subscriptions issue because of turbolinks

--- app/javascript/channels/questions_channel.js
+++ app/javascript/channels/questions_channel.js
@@ -1,12 +1,15 @@
 import consumer from "./consumer"
 
+let connected = false
+
 $(document).on('turbolinks:load', function() {
   const questions = $('.questions')
 
-  if (questions.length) {
+  if (questions.length && !connected) {
     consumer.subscriptions.create("QuestionsChannel", {
       connected() {
         this.perform('follow')
+        connected = true
       },
 
       received(data) {
