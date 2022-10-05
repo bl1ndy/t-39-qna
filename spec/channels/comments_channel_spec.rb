90d49ce9765f05eb20526183963340bd6274a598
bl1ndy <ayoblindstone@gmail.com>
2022-10-05 16:04:08 +0300

    test: add comments channel spec

---
+++ spec/channels/comments_channel_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe CommentsChannel, type: :channel do
+  it 'successfully subscribes' do
+    subscribe
+
+    expect(subscription).to be_confirmed
+  end
+end
