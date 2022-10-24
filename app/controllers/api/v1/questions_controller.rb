d2c573167ea5a50c99b5127b05b2f8ad669fad05
bl1ndy <ayoblindstone@gmail.com>
2022-10-24 23:28:06 +0300

    feat: add question create/update/destroy api

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -8,8 +8,55 @@ class Api::V1::QuestionsController < Api::V1::BaseController
   end
 
   def show
-    question = Question.find(params[:id])
-
     render json: question
+  end
+
+  def create
+    question = current_resource_owner.questions.build(question_params)
+
+    if question.save
+      render json: question, status: :created
+    else
+      render json: { errors: question.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    authorize question
+
+    if question.update(question_params)
+      render json: question, status: :created
+    else
+      render json: { errors: question.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    authorize question
+
+    if question.destroy
+      render json: question, status: :ok
+    else
+      render json: { errors: 'An error occurred while destroying Question' }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(
+      :title,
+      :body,
+      links_attributes: %i[id title url],
+      reward_attributes: %i[title image_url]
+    )
+  end
+
+  def question
+    @question ||= Question.find(params[:id])
+  end
+
+  def pundit_user
+    current_resource_owner
   end
 end
--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -4,12 +4,7 @@ require 'rails_helper'
 require_relative 'concern/api_authorizable'
 
 RSpec.describe 'Questions API', type: :request do
-  let(:headers) do
-    {
-      'CONTENT_TYPE' => 'application/json',
-      'ACCEPT' => 'application/json'
-    }
-  end
+  let(:headers) { { 'ACCEPT' => 'application/json' } }
 
   describe 'GET /api/v1/questions' do
     let(:api_path) { '/api/v1/questions' }
@@ -81,6 +76,188 @@ RSpec.describe 'Questions API', type: :request do
 
         expect(question_response['files'].size).to eq(question.files.size)
         expect(question_response['files'].first['url']).to eq(file_url)
+      end
+    end
+  end
+
+  describe 'POST /api/v1/questions' do
+    let(:api_path) { '/api/v1/questions' }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :post }
+    end
+
+    context 'with valid attributes' do
+      let(:access_token) { create(:access_token) }
+
+      it 'saves a new question in the database' do
+        expect do
+          post(api_path,
+               params: { access_token: access_token.token, question: attributes_for(:question) },
+               headers:)
+        end.to change(Question, :count).by(1)
+      end
+
+      it 'returns 201' do
+        post(api_path,
+             params: { access_token: access_token.token, question: attributes_for(:question) },
+             headers:)
+
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context 'with invalid attributes' do
+      let(:access_token) { create(:access_token) }
+
+      it 'does not save a question' do
+        expect do
+          post(api_path,
+               params: { access_token: access_token.token, question: attributes_for(:question, :invalid) },
+               headers:)
+        end.not_to change(Question, :count)
+      end
+
+      it 'returns 422' do
+        post(api_path,
+             params: { access_token: access_token.token, question: attributes_for(:question, :invalid) },
+             headers:)
+
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 'returns question errors' do
+        post(api_path,
+             params: { access_token: access_token.token, question: attributes_for(:question, :invalid) },
+             headers:)
+
+        expect(json['errors']).not_to be_empty
+      end
+    end
+  end
+
+  describe 'PATCH /api/v1/questions/:id' do
+    let(:question) { create(:question) }
+    let(:api_path) { "/api/v1/questions/#{question.id}" }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :patch }
+    end
+
+    # rubocop:disable RSpec/NestedGroups
+    describe "by question's author" do
+      let(:access_token) { create(:access_token, resource_owner_id: question.user.id) }
+
+      context 'with valid attributes' do
+        before do
+          patch(api_path,
+                params: { access_token: access_token.token, id: question, question: { body: 'Updated body' } },
+                headers:)
+        end
+
+        it 'changes question attributes' do
+          question.reload
+
+          expect(question.body).to eq('Updated body')
+        end
+
+        it 'returns 201' do
+          expect(response).to have_http_status(:created)
+        end
+      end
+
+      context 'with invalid attributes' do
+        before do
+          patch(api_path,
+                params: {
+                  access_token: access_token.token,
+                  id: question,
+                  question: attributes_for(:question, :invalid)
+                },
+                headers:)
+        end
+
+        it 'does not change question attributes' do
+          question.reload
+
+          expect(question.body).to eq('MyQuestionText')
+        end
+
+        it 'returns 422' do
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+
+        it 'returns question errors' do
+          expect(json['errors']).not_to be_empty
+        end
+      end
+    end
+    # rubocop:enable RSpec/NestedGroups
+
+    describe 'by another user' do
+      let(:access_token) { create(:access_token) }
+
+      before do
+        patch(api_path,
+              params: { access_token: access_token.token, id: question, question: { body: 'Updated body' } },
+              headers:)
+      end
+
+      it 'does not change question attributes' do
+        expect(question.body).to eq('MyQuestionText')
+      end
+
+      it 'returns 403' do
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+  end
+
+  describe 'DELETE /api/v1/questions/:id' do
+    let!(:question) { create(:question) }
+    let(:api_path) { "/api/v1/questions/#{question.id}" }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :delete }
+    end
+
+    context "when user is question's author" do
+      let(:access_token) { create(:access_token, resource_owner_id: question.user.id) }
+
+      it 'deletes the question' do
+        expect do
+          delete(api_path,
+                 params: { access_token: access_token.token, id: question },
+                 headers:)
+        end.to change(Question, :count).by(-1)
+      end
+
+      it 'returns 200' do
+        delete(api_path,
+               params: { access_token: access_token.token, id: question },
+               headers:)
+
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "when user is not question's author" do
+      let(:access_token) { create(:access_token) }
+
+      it 'does not delete the question' do
+        expect do
+          delete(api_path,
+                 params: { access_token: access_token.token, id: question },
+                 headers:)
+        end.not_to change(Question, :count)
+      end
+
+      it 'returns 403' do
+        delete(api_path,
+               params: { access_token: access_token.token, id: question },
+               headers:)
+
+        expect(response).to have_http_status(:forbidden)
       end
     end
   end
